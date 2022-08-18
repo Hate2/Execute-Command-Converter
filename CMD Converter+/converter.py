9751de56b2d363182502a314339b9cb78ee9da82
Curtidor <tannermatos18@gmail.com>
2022-08-18 16:04:34 -0400

    support for [if block, positioned and multiple same line executes]

--- CMD Converter+/converter.py
+++ CMD Converter+/converter.py
@@ -3,67 +3,79 @@
 # Published by Odin Network
 
 import os
+from typing import Iterator
 
 # TODO - Curtidor
-# add suport for detect [Half Done]
-# add suport for execute pos other than triple tilde [Not Done]
+# add support for detect [Half Done]
+# add support for execute pos other than triple tilde [Not Done]
 # fix the problem with fill summon etc [Done]
 
-###CONSTS###
-DIRECTORY_PATH = os.path.join('CMD Converter+', 'old')
+### CONSTS ###
+SOURCE_DIRECTORY_PATH = os.path.join("../CMD Converter+", 'old')
+OUT_DIRECTORY_PATH = os.path.join("../CMD Converter+", 'new')
 
 
-def convert_selector(command: str) -> [str]:
+def convert_selector(command: str) -> list[str]:
 	command = command.replace("execute @p", "execute as @p")
 	command = command.replace("execute @a", "execute as @a")
 	command = command.replace("execute @r", "execute as @r")
 	command = command.replace("execute @e", "execute as @e")
 	command = command.replace("execute @s", "execute as @s")
-
 	return command.split()
 
-def convert_tilde(command: str) -> str:
-	command = command.replace("~ ~ ~", "~~~")
-	command = command.replace("~ ~~", "~~~")
-	command = command.replace("~~ ~", "~~~")
-	return convert_selector(command)
-	
-def convert(command: str) -> str:
-	command_data = convert_tilde(command)
+
+# formats the position (x, y, z) for consistent spacing
+def formate_command(command: str) -> list[str]:
+	command_data = convert_selector(command)
+
+	index = 0
+	while index < len(command_data):
+		if '~' in command_data[index] and '~' in command_data[index + 1]:
+			command_data[index] += command_data[index + 1]
+			del command_data[index + 1]
+		elif '~' in command_data[index] and '~' in command_data[index - 1]:
+			command_data[index - 1] += command_data[index]
+			del command_data[index]
+		index += 1
+	return command_data
+
+# adds the run key word when
+def add_run(command_data: list[str], index: int, offset: int) -> None:
+	index += 1
+	while index < len(command_data):
+		if command_data[index].lstrip('-').isdigit() or '~' in command_data[index]:
+			index += 1
+		else: break
+	if not command_data[index] == 'detect': command_data[index + offset] = 'run ' + command_data[index + offset]
+
+
+def convert(command: str) -> Iterator[str]:
+	if not command.startswith('execute'): yield command.rstrip('\n')
+	command_data = formate_command(command)
 	for index, token in enumerate(command_data):
-		if token == "~~~" and command_data[index - 3] == "execute":
-			command_data[index] = "at @s run"
+		if token == 'execute' or token == 'run execute':
+			if command_data[index+3] == '~~~':
+				command_data[index+3] = command_data[index+3].replace('~~~', 'at @s run')
+			else:
+				command_data[index+3] = 'positioned ' + command_data[index+3]
+				add_run(command_data, index+3, 0)
 		elif token == 'detect':
-			command_data[index-1] = command_data[index-1].replace(' run', '')
 			command_data[index] = 'if block'
+			if command_data[index - 1] == 'at @s run': command_data[index-1] = 'at @s'
+			add_run(command_data, index, 2)
+	print(command_data)
+	yield " ".join(command_data)
 
-	return " ".join(command_data)
-
-
-def show(total_converted: int, total_not: int, file: str, line_count: int) -> None:
-    print(f"{'-'.rjust(20,'-')} \n{file} was converted.")
-    print(
-        f"Execute Commands: {total_converted}\nSkipped Lines:{total_not}\nTotal Lines: {line_count}\n"
-    )
 
 def main():
-	for file_name in os.listdir(DIRECTORY_PATH):
-		if file_name.endswith("mcfunction"):
-			with open(os.path.join("CMD Converter+/new", file_name), "w") as out_file:
-				with open(os.path.join("CMD Converter+/old", file_name), "r") as reader:
-					exe_commands = 0
-					non_exe = 0
-					for line_count, command in enumerate(reader):
-						if "execute" in command:
-							exe_commands = 1
-							out_file.write(convert(command))
-							out_file.write('\n')
-						else:
-							non_exe = 1
-							out_file.write(command)
-				show(exe_commands, non_exe, file_name, line_count)
-	
-	close = input("----------------\nType anything to close...")
+	for file_name in os.listdir(SOURCE_DIRECTORY_PATH):
+		if not file_name.endswith("mcfunction"): continue
+		with open(os.path.join(SOURCE_DIRECTORY_PATH, file_name), "r") as source_file:
+			with open(os.path.join(OUT_DIRECTORY_PATH, file_name), "w") as out_file:
+				for command in source_file:
+					out_file.write(next(convert(command)))
+					out_file.write('\n')
+
 
 if __name__ == "__main__":
 	main()
